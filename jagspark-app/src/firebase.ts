fff0b54b93277361e0116998fa71990d9b848a9e
kafoulk <kafoulk@iu.edu>
2026-04-08 08:33:35 -0400

    ADDED .ENV.LOCAL

--- jagspark-app/src/firebase.ts
+++ jagspark-app/src/firebase.ts
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDgsYtLr2LUwmDqVQyaKeDjNmOkxrSBO_g",
-  authDomain: "jagspark-app.firebaseapp.com",
-  projectId: "jagspark-app",
-  storageBucket: "jagspark-app.firebasestorage.app",
-  messagingSenderId: "347385840267",
-  appId: "1:347385840267:web:909c57ad54dbe55c5c0a8e",
-  measurementId: "G-CJNJYFPJYP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
